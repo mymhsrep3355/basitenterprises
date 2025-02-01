61bafa6e37844ce82b497df1615ff5d687b62b1a
muhammad <muhammadhamzasalahuddin@gmail.com>
2025-02-01 17:26:25 +0500

    final - changes

--- src/themes/theme.js
+++ src/themes/theme.js
@@ -1,10 +1,30 @@
 // theme.js
 import { extendTheme } from "@chakra-ui/react";
+import "../../src/index.css"
 
 const theme = extendTheme({
   fonts: {
     heading: "'Bebas Neue', 'Poppins', '', sans-serif", 
     body: "'Playfair Display', 'Poppins', sans-serif", 
+  },
+  styles: {
+    global: {
+      "::-webkit-scrollbar": {
+        width: "25px",
+      },
+      "::-webkit-scrollbar-track": {
+        background: "white",
+        borderRadius: "20px",
+      },
+      "::-webkit-scrollbar-thumb": {
+        background: "linear-gradient(135deg, #C02232, #d62828)",
+        borderRadius: "20px",
+      },
+      html: {
+        scrollbarWidth: "thin",
+        scrollbarColor: "#C02232 #f4f4f4",
+      },
+    },
   },
 });
 
